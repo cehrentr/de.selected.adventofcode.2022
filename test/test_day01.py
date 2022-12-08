fb31082eac0efab1f19a4460b5ddf22454a4bb91
Christian Ehrentraut <github@pimpslap.de>
2022-12-08 16:22:27 +0100

    Solution of day 1

---
+++ test/test_day01.py
@@ -0,0 +1,34 @@
+import unittest
+from day_01.solution import AdventOfCodePuzzleDay01
+
+
+class TestAdventOfCodePuzzleDay01(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.input_day01_example = []
+        with open("./testdata/input_day01_example.txt", "r") as fil:
+            self.input_day01_example = [line.strip() for line in fil]
+
+        self.input_day01 = []
+        with open("./testdata/input_day01.txt", "r") as fil:
+            self.input_day01 = [line.strip() for line in fil]
+
+    def test_solution_day_01_puzzle_1_example(self):
+        solution = AdventOfCodePuzzleDay01(self.input_day01_example).solve_puzzle_1()
+        self.assertEqual(solution, 24000)
+
+    def test_solution_day_01_puzzle_1(self):
+        solution = AdventOfCodePuzzleDay01(self.input_day01).solve_puzzle_1()
+        self.assertEqual(solution, 70374)
+
+    def test_solution_day_01_puzzle_2_example(self):
+        solution = AdventOfCodePuzzleDay01(self.input_day01_example).solve_puzzle_2()
+        self.assertEqual(solution, 45000)
+
+    def test_solution_day_01_puzzle_2(self):
+        solution = AdventOfCodePuzzleDay01(self.input_day01).solve_puzzle_2()
+        self.assertEqual(solution, 204610)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=True)
